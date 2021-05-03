7f640dc39baed281e3edf2f67de7177c5a0dc7ab
Daniel <danielliu1824@gmail.com>
2021-05-03 16:36:51 -0700

    Fetched row posters, added some css

--- netflix-clone/src/Row.jsx
+++ netflix-clone/src/Row.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Row ({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
 
@@ -13,11 +15,21 @@ function Row ({ title, fetchURL }) {
     fetchData();
   }, [fetchURL])
 
-  console.log(movies)
+  console.table(movies)
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
 
+      <div className="row_posters">
+        {/* several row_posters */}
+        {movies.map(movie => (
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   )
 }
