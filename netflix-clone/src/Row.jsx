2db9ab232aa3b3bb223f9c1692a82612075e65ba
Daniel <danielliu1824@gmail.com>
2021-05-03 16:22:17 -0700

    Fetched movies from API

--- netflix-clone/src/Row.jsx
+++ netflix-clone/src/Row.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-function Row ({ title }) {
+function Row ({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    async function fetchData () {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL])
+
+  console.log(movies)
   return (
     <div>
       <h2>{title}</h2>
-
 
     </div>
   )
