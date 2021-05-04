1fe8e2c6039510777713e4c04dc2e07ebe0a5cce
Daniel <danielliu1824@gmail.com>
2021-05-03 17:53:23 -0700

    More banner css, added truncate function to reduce banner description text

--- netflix-clone/src/Banner.jsx
+++ netflix-clone/src/Banner.jsx
@@ -18,6 +18,10 @@ function Banner() {
     fetchData();
   }, []);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "...": str;
+  }
+
   return (
     <header className="banner"
       style={{
@@ -29,14 +33,14 @@ function Banner() {
       }}
     >
       <div className="banner_contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner_title">{movie?.title || movie?.name || movie?.original_name}</h1>
 
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
 
-        <h1 className="banner_description>">{movie?.overview}</h1>
+        <h1 className="banner_description">{movie?.overview}</h1>
       </div>
 
     </header>
